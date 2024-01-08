d706aae09333513f208e161b91452e4b159440e6
Ayu <claire@ayu.land>
2024-01-08 15:40:48 +0800

    Breathing lights

---
+++ testdrive/fw/debug/run.sh
@@ -0,0 +1,20 @@
+# ~/.platformio/packages/tool-openocd/bin/openocd -f interface/stlink.cfg -f target/stm32g0x.cfg
+# (cd ~/Downloads/stlink; build/Release/bin/st-info --probe)
+
+cat >debug/gdbinit <<EOF
+define hook-quit
+  set confirm off
+end
+target extended-remote localhost:3333
+
+b swv_trap_line
+commands
+  silent
+  printf "%s\n", swv_buf
+  c
+end
+r
+EOF
+
+~/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-gdb .pio/build/dev/firmware.elf -x debug/gdbinit
+rm debug/gdbinit
