7a76a62b9b8226de40eb42592fda0bf3f01950d4
Ayu <claire@ayu.land>
2024-01-09 16:15:16 +0800

    Add fixed-point version of the filter

--- testdrive/filter_test.c
+++ testdrive/filter_test.c
@@ -2,6 +2,9 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#define FIXED_POINT 1
+
+#if !FIXED_POINT
 struct filter {
   float i1, i2;
 };
@@ -19,6 +22,30 @@ static inline float filter_update(
   return f->i2;
 }
 
+#else
+struct filter {
+  int32_t i1, i2;
+};
+
+// 8.24 fixed point
+// Tick is 1/100 s = 167772
+static inline int32_t filter_update(
+  struct filter *f,
+  int32_t th_a, int32_t dth_g
+) {
+  int32_t x1 = f->i2 - th_a;
+  f->i1 += ((int64_t)x1 * 167772) >> 24;
+  const int32_t omega = 3.0f;
+  int32_t x2 =
+    (((int64_t)f->i1 * 50331648) >> 24) + // omega
+    (((int64_t)x1 * 71179699) >> 24); // sqrt(2) * omega
+  int32_t x3 = dth_g - x2;
+  f->i2 += ((int64_t)x3 * 167772) >> 24;
+  return f->i2;
+}
+
+#endif
+
 static inline uint32_t myrand()
 {
   static uint32_t seed = 240109;
@@ -28,7 +55,7 @@ static inline uint32_t myrand()
 int main()
 {
   struct filter f = { 0 };
-  for (int i = 0; i < 5000; i++) {
+  for (int i = 0; i < 5100; i++) {
     float t = (float)i / 100;
     float freq_Hz = 0.1f;
     // 30% amplitude high-frequency white noise
@@ -43,7 +70,11 @@ int main()
     float gnoise = gnoise_sum / 300 * 10;
     float a = 9.99 * sinf(t * 6.2832 * freq_Hz) + anoise;
     float g = 9.99 * cosf(t * 6.2832 * freq_Hz) * (6.2832 * freq_Hz) + gnoise;
+  #if !FIXED_POINT
     float th = filter_update(&f, a, g);
+  #else
+    float th = (float)filter_update(&f, a * 16777216, g * 16777216) / 16777216;
+  #endif
     printf("%4d %9.5f | %9.5f (%8.5f) %9.5f (%8.5f) | %8.5f\n", i, th,
       a, anoise, g, gnoise, 9.99 * sinf(t * 6.2832 * freq_Hz));
   }
