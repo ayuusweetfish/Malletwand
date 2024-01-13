8e392fea87aafbf56b1e7fde49acfda142ce561e
Ayu <claire@ayu.land>
2024-01-13 23:47:17 +0800

    Read data from magnetic sensor

--- testdrive/fw/debug/run.sh
+++ testdrive/fw/debug/run.sh
@@ -1,10 +1,17 @@
-# ~/.platformio/packages/tool-openocd/bin/openocd -f interface/stlink.cfg -f target/stm32g0x.cfg
+# ~/.platformio/packages/tool-openocd/bin/openocd -f interface/stlink.cfg -f target/stm32g0x.cfg -d1
 # (cd ~/Downloads/stlink; build/Release/bin/st-info --probe)
 
 cat >debug/gdbinit <<EOF
 define hook-quit
   set confirm off
 end
+define hook-run
+  set confirm off
+end
+define hookpost-run
+  set confirm on
+end
+set pagination off
 target extended-remote localhost:3333
 
 b swv_trap_line
--- testdrive/fw/src/main.c
+++ testdrive/fw/src/main.c
@@ -15,7 +15,7 @@
 #define nRF_CE_PIN  GPIO_PIN_4
 #define nRF_CE_PORT GPIOA
 
-#define RELEASE
+// #define RELEASE
 #ifndef RELEASE
 static uint8_t swv_buf[256];
 static size_t swv_buf_ptr = 0;
@@ -908,6 +908,48 @@ int main()
   bmi270_write_reg(0x42, 0xA9);   // GYR_CONF
   bmi270_write_reg(0x43, 0x00);   // GYR_RANGE: +/-2000dps
   HAL_Delay(10);
+
+  bmi270_write_reg(0x6B, 0x21);   // IF_CONF.aux_en = 1
+  bmi270_write_reg(0x7D, 0b0110); // PWR_CTRL.aux_en = 0
+  bmi270_write_reg(0x4B, 0x30 << 1);  // AUX_DEV_ID
+  bmi270_write_reg(0x4C, 0b11001010);
+    // AUX_IF_CONF
+    //   .aux_rd_burst = 0x2 (length 6)
+    //   .man_rd_burst = 0x2 (length 6)
+    //   .aux_manual_en = 1
+/*
+  // Write to MMC5603NJ register 0x1B (Internal Control 0), value 0x21
+  bmi270_write_reg(0x4F, 0x21);   // AUX_WR_DATA
+  bmi270_write_reg(0x4E, 0x1B);   // AUX_WR_ADDR
+  HAL_Delay(1000);
+  bmi270_write_reg(0x4D, 0x00);   // AUX_RD_ADDR
+*/
+  // Write to ODR (0x1A), value 100
+  bmi270_write_reg(0x4F, 100);
+  bmi270_write_reg(0x4E, 0x1A);
+  HAL_Delay(1);
+  // Write to Internal Control 0 (0x1B), value 0x81
+  bmi270_write_reg(0x4F, 0x81);
+  bmi270_write_reg(0x4E, 0x1B);
+  HAL_Delay(1);
+  // Write to Internal Control 2 (0x1D), value 0x10
+  bmi270_write_reg(0x4F, 0x10);
+  bmi270_write_reg(0x4E, 0x1D);
+  HAL_Delay(1000);
+
+  while (1) {
+    uint8_t data[32] = {1, 2, 3, 4, 5, 6, 7, 8};
+    bmi270_write_reg(0x4D, 0x00);   // AUX_RD_ADDR
+    HAL_Delay(1);
+    bmi270_read_burst(0x04, data, 8);
+    uint16_t aux_x = ((uint16_t)data[0] << 8) | data[1];
+    uint16_t aux_y = ((uint16_t)data[2] << 8) | data[3];
+    uint16_t aux_z = ((uint16_t)data[4] << 8) | data[5];
+    uint16_t aux_r = ((uint16_t)data[6] << 8) | data[7];
+    swv_printf("aux = %04x %04x %04x %04x\n",
+      (unsigned)aux_x, (unsigned)aux_y, (unsigned)aux_z, (unsigned)aux_r);
+    HAL_Delay(100);
+  }
 
   // ======== Main loop ========
 
