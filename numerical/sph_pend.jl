0697546fc934cfc0a0a80cb1ef6fd0f336f66dd6
Ayu <claire@ayu.land>
2024-01-20 00:51:13 +0800

    Plot spherical integrations in small-angle cases

--- numerical/sph_pend.jl
+++ numerical/sph_pend.jl
@@ -11,29 +11,26 @@ RK4(f, x, dt) = begin
   (k1 + 2 * k2 + 2 * k3 + k4) / 6 * dt
 end
 
-θ = 0.4
+θ = 0.1
 ϕ = 0.0
 θd = 0.0
-ϕd = 0.5
+ϕd = 0.01
 k = 1.5
 
 x = [θ, ϕ, θd, ϕd]
-p = Observable(Point3f[])
-vs = Observable(repeat([0.0], 5000))
-pLast = Observable(Point3f(0))
+p = Point3f[]
+θs = Float32[]
+ϕs = Float32[]
+vs = Float32[]
+xs = Float32[]
+ys = Float32[]
+zs = Float32[]
+pLast = Point3f(0)
 
-fig = Figure(size = (1200, 1200))
-ax1 = Axis3(fig[1, 1])
-scatter!(ax1, [Point3f(-1), Point3f(0), Point3f(1)])
-scatter!(ax1, @lift([$pLast]))
-lines!(ax1, p)
+using LinearAlgebra: norm
 
-ax2 = Axis(fig[1, 2])
-lines!(ax2, vs)
-display(fig)
-
-for i in 1:5000
-  dt = 0.01
+for i in 1:10000
+  dt = 0.001
   global x
   x += RK4(x, dt) do(x)
     θ, ϕ, θd, ϕd = x
@@ -43,8 +40,29 @@ for i in 1:5000
   end
   local θ, ϕ, θd, ϕd = x
   pCur = Point3f(sin(θ)*cos(ϕ), sin(θ)*sin(ϕ), -cos(θ))
-  push!(p[], pCur); p[] = p[]
-  pLast[] = pCur
-  vs[][i] = sqrt(θd^2 + sin(θ)^2 * ϕd^2); vs[] = vs[]
-  sleep(0.001)
+  push!(p, pCur)
+  push!(θs, θ)
+  push!(ϕs, ϕ % π)
+  # push!(vs, sqrt(θd^2 + sin(θ)^2 * ϕd^2))
+  push!(vs, if i == 1 0 else norm(pCur .- pLast, 2) / dt end)
+  global pLast = pCur
+  push!(xs, pCur[1])
+  push!(ys, pCur[2])
+  push!(zs, pCur[3])
 end
+
+fig = Figure(size = (1200, 1200))
+ax1 = Axis3(fig[1, 1])
+scatter!(ax1, [Point3f(-1), Point3f(0), Point3f(1)])
+lines!(ax1, p)
+
+ax2 = Axis(fig[2, 1])
+lθ = lines!(ax2, θs)
+lϕ = lines!(ax2, ϕs)
+lv = lines!(ax2, vs)
+lx = lines!(ax2, xs)
+ly = lines!(ax2, ys)
+lz = lines!(ax2, zs)
+Legend(fig[2, 2], [lθ, lϕ, lv, lx, ly, lz], ["θ", "ϕ", "v", "x", "y", "z"])
+
+wait(display(fig))
