934c53ae353bd3af4c7a1a630a208577e5342f31
Ayu <claire@ayu.land>
2024-01-19 23:24:00 +0800

    Plot velocity of spherical pendulum

--- numerical/sph_pend.jl
+++ numerical/sph_pend.jl
@@ -11,14 +11,15 @@ RK4(f, x, dt) = begin
   (k1 + 2 * k2 + 2 * k3 + k4) / 6 * dt
 end
 
-θ = 1.0
+θ = 0.1
 ϕ = 0.0
 θd = 0.0
-ϕd = 0.4
+ϕd = 0.1
 k = 1.5
 
 x = [θ, ϕ, θd, ϕd]
 p = Point3f[]
+vs = Float32[]
 
 for i in 1:5000
   dt = 0.01
@@ -31,10 +32,14 @@ for i in 1:5000
   end
   local θ, ϕ, θd, ϕd = x
   push!(p, Point3f(sin(θ)*cos(ϕ), sin(θ)*sin(ϕ), -cos(θ)))
+  push!(vs, sqrt(θd^2 + sin(θ)^2 * ϕd^2))
 end
 
 fig = Figure(size = (1200, 1200))
-ax = Axis3(fig[1, 1])
-scatter!(ax, [Point3f(-1), Point3f(0), Point3f(1)])
-lines!(ax, p)
+ax1 = Axis3(fig[1, 1])
+scatter!(ax1, [Point3f(-1), Point3f(0), Point3f(1)])
+lines!(ax1, p)
+
+ax2 = Axis(fig[2, 1])
+lines!(ax2, vs)
 wait(display(fig))
