899e2ff3a9994adac73b44a1a640203926fd49b0
Ayu <claire@ayu.land>
2024-01-20 17:39:00 +0800

    Calibrate accelerometer w.r.t. gravity

---
+++ testdrive/cli/geom.h
@@ -0,0 +1,158 @@
+#include <math.h>
+
+typedef struct { float x, y; } vec2;
+typedef struct { float x, y, z; } vec3;
+typedef struct { float x, y, z, w; } quat;
+
+static inline vec3 vec3_add(vec3 a, vec3 b) {
+  return (vec3){a.x + b.x, a.y + b.y, a.z + b.z};
+}
+static inline vec3 vec3_diff(vec3 a, vec3 b) {
+  return (vec3){a.x - b.x, a.y - b.y, a.z - b.z};
+}
+static inline vec3 vec3_scale(vec3 a, float k) {
+  return (vec3){a.x * k, a.y * k, a.z * k};
+}
+static inline float vec3_dot(vec3 a, vec3 b) {
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+static inline float vec3_norm(vec3 a) { return sqrtf(vec3_dot(a, a)); }
+static inline float vec3_dist(vec3 a, vec3 b) {
+  return vec3_norm(vec3_diff(a, b));
+}
+static inline float vec3_distsq(vec3 a, vec3 b) {
+  return vec3_dot(vec3_diff(a, b), vec3_diff(a, b));
+}
+static inline vec3 vec3_cross(vec3 a, vec3 b) {
+  return (vec3){
+    +(a.y * b.z - b.y * a.z),
+    -(a.x * b.z - b.x * a.z),
+    +(a.x * b.y - b.x * a.y),
+  };
+}
+static inline vec3 vec3_lerp(vec3 a, vec3 b, float t) {
+  return (vec3){
+    a.x + (b.x - a.x) * t,
+    a.y + (b.y - a.y) * t,
+    a.z + (b.z - a.z) * t
+  };
+}
+static inline vec3 vec3_slerp(vec3 a, vec3 b, float t) {
+  float o = acosf(vec3_dot(a, b));
+  return vec3_add(
+    vec3_scale(a, sinf((1 - t) * o) / sinf(o)),
+    vec3_scale(b, sinf(t * o) / sinf(o))
+  );
+}
+static inline vec3 vec3_normalize(vec3 a) {
+  float norm = sqrtf(a.x * a.x + a.y * a.y + a.z * a.z);
+  return (vec3){a.x / norm, a.y / norm, a.z / norm};
+}
+static inline quat quat_inv(quat q) {
+  return (quat){-q.x, -q.y, -q.z, q.w};
+}
+static inline quat quat_scale(quat q, float k) {
+  return (quat){k*q.x, k*q.y, k*q.z, k*q.w};
+}
+static inline float quat_norm(quat q) {
+  return sqrtf(q.x*q.x + q.y*q.y + q.z*q.z + q.w*q.w);
+}
+static inline quat quat_normalize(quat q) {
+  return quat_scale(q, 1.f / quat_norm(q));
+}
+static inline quat quat_add(quat p, quat q) {
+  return (quat){p.x+q.x, p.y+q.y, p.z+q.z, p.w+q.w};
+}
+static inline quat quat_mul(quat p, quat q) {
+  return (quat){
+    p.w * q.x + q.w * p.x + (p.y * q.z - q.y * p.z),
+    p.w * q.y + q.w * p.y - (p.x * q.z - q.x * p.z),
+    p.w * q.z + q.w * p.z + (p.x * q.y - q.x * p.y),
+    p.w * q.w - (p.x * q.x + p.y * q.y + p.z * q.z)
+  };
+}
+static inline vec3 quat_rot(quat q, vec3 v) {
+  quat r = quat_mul(quat_mul(q, (quat){v.x, v.y, v.z, 0}), quat_inv(q));
+  return (vec3){r.x, r.y, r.z};
+}
+static inline vec3 vec_rot(vec3 v, vec3 axis, float angle) {
+  quat q = (quat){
+    axis.x * sinf(angle / 2),
+    axis.y * sinf(angle / 2),
+    axis.z * sinf(angle / 2),
+    cosf(angle / 2)
+  };
+  return quat_rot(q, v);
+}
+static inline quat rot_from_vecs(vec3 xto, vec3 yto, vec3 zto) {
+  float M[3][3] = {
+    {xto.x, yto.x, zto.x},
+    {xto.y, yto.y, zto.y},
+    {xto.z, yto.z, zto.z},
+  };
+  // Largest diagonal entry
+  float maxd = -2;
+  int maxdi;
+  for (int i = 0; i < 3; i++)
+    if (maxd < M[i][i]) { maxd = M[i][i]; maxdi = i; }
+  float r = sqrtf(1 - M[0][0] - M[1][1] - M[2][2] + maxd * 2);
+  float w, x, y, z;
+  if (maxdi == 0) {
+    w = (M[2][1] - M[1][2]) / (r * 2);
+    x = r / 2;
+    y = (M[0][1] + M[1][0]) / (r * 2);
+    z = (M[0][2] + M[2][0]) / (r * 2);
+  } else if (maxdi == 1) {
+    w = (M[0][2] - M[2][0]) / (r * 2);
+    x = (M[1][0] + M[0][1]) / (r * 2);
+    y = r / 2;
+    z = (M[1][2] + M[2][1]) / (r * 2);
+  } else {
+    w = (M[1][0] - M[0][1]) / (r * 2);
+    x = (M[2][0] + M[0][2]) / (r * 2);
+    y = (M[2][1] + M[1][2]) / (r * 2);
+    z = r / 2;
+  }
+  return (quat){x, y, z, w};
+}
+static inline quat rot_from_view(vec3 lookat, vec3 right) {
+  return rot_from_vecs(
+    right,
+    vec3_cross(right, lookat),
+    vec3_scale(lookat, -1)
+  );
+}
+static inline quat rot_from_endpoints(vec3 u, vec3 v) {
+  vec3 a = vec3_cross(u, v);
+  float w = sqrtf(vec3_dot(u, u) * vec3_dot(v, v)) + vec3_dot(u, v);
+  quat q = (quat){a.x, a.y, a.z, w};
+  return quat_normalize(q);
+}
+static inline quat quat_exp(quat q) {
+  float e = expf(q.w);
+  vec3 v = (vec3){q.x, q.y, q.z};
+  float vnorm = vec3_norm(v);
+  float w = e * cosf(vnorm);
+  v = vec3_scale(v, e * (fabsf(vnorm) < 1e-4 ? 1 : sinf(vnorm) / vnorm));
+  return (quat){v.x, v.y, v.z, w};
+}
+static inline quat quat_log(quat q) {
+  // Caveat: does not handle pure quaternions
+  float qnorm = quat_norm(q);
+  vec3 v = (vec3){q.x, q.y, q.z};
+  float vnorm = vec3_norm(v);
+  float w = logf(qnorm);
+  v = vec3_scale(v, acosf(q.w / qnorm) / vnorm);
+  return (quat){v.x, v.y, v.z, w};
+}
+static inline quat quat_pow(quat q, float t) {
+  quat l = quat_log(q);
+  return quat_exp(quat_scale(l, t));
+}
+static inline quat quat_minorarc(quat from, quat to) {
+  quat q = quat_mul(to, quat_inv(from));
+  return q.w < 0 ? quat_scale(q, -1) : q;
+}
+static inline quat quat_slerp(quat a, quat b, float t) {
+  return quat_mul(quat_pow(quat_minorarc(a, b), t), a);
+}
--- testdrive/cli/main.cc
+++ testdrive/cli/main.cc
@@ -1,5 +1,7 @@
 // c++ % -ISimpleBLE/simpleble/include -Ibuild_simpleble/export build_simpleble/lib/libsimpleble.a -Iraylib-5.0_macos/include raylib-5.0_macos/lib/libraylib.a -std=c++17 -framework CoreBluetooth -framework Foundation -framework CoreGraphics -framework IOKit -framework AppKit
 
+#include "geom.h"
+
 #include "simpleble/SimpleBLE.h"
 namespace rl {
   #include "raylib.h"
@@ -16,10 +18,11 @@ namespace rl {
 #endif
 
 #include <mutex>
+#include <queue>
 #include <set>
 
 std::mutex readings_mutex;
-int16_t mag[3], acc[3], gyr[3];
+int16_t mag_out[3], acc_out[3], gyr_out[3];
 
 int main() {
   if (!SimpleBLE::Adapter::bluetooth_enabled()) {
@@ -87,15 +90,15 @@ int main() {
       for (int i = 0; i < y.length(); i++) payload[i + 2] = (uint8_t)y[i];
       for (int i = 0; i < y.length() + 2; i++) printf(" %02x", payload[i]); putchar('\n');
       readings_mutex.lock();
-      mag[0] = ((int16_t)payload[ 0] << 8) | payload[ 1];
-      mag[1] = ((int16_t)payload[ 2] << 8) | payload[ 3];
-      mag[2] = ((int16_t)payload[ 4] << 8) | payload[ 5];
-      acc[0] = ((int16_t)payload[ 6] << 8) | payload[ 7];
-      acc[1] = ((int16_t)payload[ 8] << 8) | payload[ 9];
-      acc[2] = ((int16_t)payload[10] << 8) | payload[11];
-      gyr[0] = ((int16_t)payload[12] << 8) | payload[13];
-      gyr[1] = ((int16_t)payload[14] << 8) | payload[15];
-      gyr[2] = ((int16_t)payload[16] << 8) | payload[17];
+      mag_out[0] = ((int16_t)payload[ 0] << 8) | payload[ 1];
+      mag_out[1] = ((int16_t)payload[ 2] << 8) | payload[ 3];
+      mag_out[2] = ((int16_t)payload[ 4] << 8) | payload[ 5];
+      acc_out[0] = ((int16_t)payload[ 6] << 8) | payload[ 7];
+      acc_out[1] = ((int16_t)payload[ 8] << 8) | payload[ 9];
+      acc_out[2] = ((int16_t)payload[10] << 8) | payload[11];
+      gyr_out[0] = ((int16_t)payload[12] << 8) | payload[13];
+      gyr_out[1] = ((int16_t)payload[14] << 8) | payload[15];
+      gyr_out[2] = ((int16_t)payload[16] << 8) | payload[17];
       readings_mutex.unlock();
     }
   };
@@ -104,6 +107,9 @@ int main() {
 
   printf("Starting scan\n");
   adapter.scan_start();
+
+  std::deque<vec3> mag_history;
+  quat q_ref = (quat){0, 0, 0, 1};
 
   const int W = 1200;
   const int H = 640;
@@ -120,8 +126,21 @@ int main() {
   while (!rl::WindowShouldClose()) {
     rl::BeginDrawing();
       rl::ClearBackground((rl::Color){255, 254, 253, 255});
+
       if (rl::IsKeyDown(rl::KEY_LEFT_SHIFT) || rl::IsMouseButtonDown(rl::MOUSE_BUTTON_LEFT))
         rl::UpdateCamera(&camera, rl::CAMERA_THIRD_PERSON);
+
+      vec3 acc = (vec3){(float)acc_out[0], (float)acc_out[1], (float)acc_out[2]};
+      vec3 gyr = (vec3){(float)gyr_out[0], (float)gyr_out[1], (float)gyr_out[2]};
+      vec3 mag = (vec3){(float)mag_out[0], (float)mag_out[1], (float)mag_out[2]};
+
+      if (rl::IsKeyPressed(rl::KEY_SPACE)) {
+        q_ref = rot_from_endpoints(acc, (vec3){0, 0, 1});
+      }
+      acc = quat_rot(q_ref, acc);
+      gyr = quat_rot(q_ref, gyr);
+      mag = quat_rot(q_ref, mag);
+
       rl::BeginMode3D(camera);
         // maths (x, y, z) -> screen (x, -z, y)
         // screen (x, y, z) -> maths (x, z, -y)
@@ -140,35 +159,41 @@ int main() {
         //   24, 24, (rl::Color){255, 0, 0, 128});
         // rl::DrawGrid(10, 1);
 
-        auto plotPoint = [] (float x, float y, float z, rl::Color c) -> void {
+        auto plotPoint = [] (vec3 v, rl::Color c, bool ref = true) -> void {
+          float x = v.x, y = v.y, z = v.z;
           rl::Vector3 p = (rl::Vector3){x, z, -y};
-          rl::Vector3 px = (rl::Vector3){x, 0, 0};
-          rl::Vector3 py = (rl::Vector3){0, z, 0};
-          rl::Vector3 pz = (rl::Vector3){0, 0, -y};
           rl::DrawSphere(p, 0.04, c);
-          rl::DrawSphere(px, 0.02, c);
-          rl::DrawSphere(py, 0.02, c);
-          rl::DrawSphere(pz, 0.02, c);
-          // rl::Vector3 pyz = (rl::Vector3){0, z, -y};
-          // rl::Vector3 pzx = (rl::Vector3){x, 0, -y};
-          // rl::Vector3 pxy = (rl::Vector3){x, z, 0};
-          // rl::DrawLine3D(p, pxy, c);
-          rl::DrawCubeWiresV((rl::Vector3){p.x/2, p.y/2, p.z/2}, p, c);
+          if (ref) {
+            rl::Vector3 px = (rl::Vector3){x, 0, 0};
+            rl::Vector3 py = (rl::Vector3){0, z, 0};
+            rl::Vector3 pz = (rl::Vector3){0, 0, -y};
+            rl::DrawSphere(px, 0.02, c);
+            rl::DrawSphere(py, 0.02, c);
+            rl::DrawSphere(pz, 0.02, c);
+            rl::DrawCubeWiresV((rl::Vector3){p.x/2, p.y/2, p.z/2}, p, c);
+          }
         };
 
         float accScale = 1.f / 8192;  // unit = 1g
         plotPoint(
-          (float)acc[0] * accScale, (float)acc[1] * accScale, (float)acc[2] * accScale,
+          vec3_scale(acc, accScale),
           (rl::Color){120, 60, 0, 255});
 
         float gyrScale = 1.f / (16.384 * 360);  // unit = 1 rps
         plotPoint(
-          (float)gyr[0] * gyrScale, (float)gyr[1] * gyrScale, (float)gyr[2] * gyrScale,
+          vec3_scale(gyr, gyrScale),
           (rl::Color){150, 20, 210, 255});
 
         float magScale = 2.f / 1024;  // unit = 0.5 G = 0.05 mT ≈ geomagnetic field
+        mag_history.push_back(mag);
+        if (mag_history.size() >= 500) mag_history.pop_front();
+        if (0) for (const vec3 p : mag_history) {
+          plotPoint(
+            vec3_scale(p, magScale),
+            (rl::Color){24, 20, 180, 255}, false);
+        }
         plotPoint(
-          (float)mag[0] * magScale, (float)mag[1] * magScale, (float)mag[2] * magScale,
+          vec3_scale(mag, magScale),
           (rl::Color){24, 20, 180, 255});
       rl::EndMode3D();
     rl::EndDrawing();
