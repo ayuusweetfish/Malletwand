138ee5827a6339bf0482cdd9dedf71bd4dc39ee6
Ayu <claire@ayu.land>
2024-01-22 20:12:25 +0800

    Run firmware on the wand hardware (Wand Rev. 2)

---
+++ wand/fw/debug/run.sh
@@ -0,0 +1,27 @@
+# ~/.platformio/packages/tool-openocd/bin/openocd -f interface/stlink.cfg -f target/stm32g0x.cfg -d1
+# (cd ~/Downloads/stlink; build/Release/bin/st-info --probe)
+
+cat >debug/gdbinit <<EOF
+define hook-quit
+  set confirm off
+end
+define hook-run
+  set confirm off
+end
+define hookpost-run
+  set confirm on
+end
+set pagination off
+target extended-remote localhost:3333
+
+b swv_trap_line
+commands
+  silent
+  printf "%s\n", (char *)swv_buf
+  c
+end
+r
+EOF
+
+~/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-gdb .pio/build/dev/firmware.elf -x debug/gdbinit
+rm debug/gdbinit
