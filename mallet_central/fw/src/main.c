81402b0d2b4204c58ae30d14a2ea58fc0106842e
Ayu <claire@ayu.land>
2024-01-30 15:53:01 +0800

    Add firmware for the central unit, blinking LED

---
+++ mallet_central/fw/src/main.c
@@ -0,0 +1,143 @@
+#include <stm32g0xx_hal.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LED_IND_ACT_PORT  GPIOA
+#define LED_IND_ACT_PIN   GPIO_PIN_8
+
+I2C_HandleTypeDef i2c2;
+
+static uint8_t swv_buf[256];
+static size_t swv_buf_ptr = 0;
+__attribute__ ((noinline, used))
+void swv_trap_line()
+{
+  *(volatile char *)swv_buf;
+}
+static inline void swv_putchar(uint8_t c)
+{
+  // ITM_SendChar(c);
+  if (c == '\n') {
+    swv_buf[swv_buf_ptr >= sizeof swv_buf ?
+      (sizeof swv_buf - 1) : swv_buf_ptr] = '\0';
+    swv_trap_line();
+    swv_buf_ptr = 0;
+  } else if (++swv_buf_ptr <= sizeof swv_buf) {
+    swv_buf[swv_buf_ptr - 1] = c;
+  }
+}
+static void swv_printf(const char *restrict fmt, ...)
+{
+  char s[256];
+  va_list args;
+  va_start(args, fmt);
+  int r = vsnprintf(s, sizeof s, fmt, args);
+  for (int i = 0; i < r && i < sizeof s - 1; i++) swv_putchar(s[i]);
+  if (r >= sizeof s) {
+    for (int i = 0; i < 3; i++) swv_putchar('.');
+    swv_putchar('\n');
+  }
+}
+
+int main()
+{
+  HAL_Init();
+
+  // ======== GPIO ========
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  GPIO_InitTypeDef gpio_init;
+
+  gpio_init.Pin = LED_IND_ACT_PIN;
+  gpio_init.Mode = GPIO_MODE_OUTPUT_PP;
+  gpio_init.Pull = GPIO_PULLDOWN;
+  gpio_init.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_IND_ACT_PORT, &gpio_init);
+  HAL_GPIO_WritePin(LED_IND_ACT_PORT, LED_IND_ACT_PIN, 1);
+
+  // SWD (PA13, PA14)
+  gpio_init.Pin = GPIO_PIN_13 | GPIO_PIN_14;
+  gpio_init.Mode = GPIO_MODE_AF_PP; // Pass over control to AF peripheral
+  gpio_init.Alternate = GPIO_AF0_SWJ;
+  gpio_init.Pull = GPIO_PULLUP;
+  gpio_init.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &gpio_init);
+
+  // ======== Clocks ========
+  RCC_OscInitTypeDef osc_init = { 0 };
+  osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  osc_init.HSIState = RCC_HSI_ON;
+  osc_init.PLL.PLLState = RCC_PLL_ON;
+  osc_init.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  osc_init.PLL.PLLM = RCC_PLLM_DIV2;
+  osc_init.PLL.PLLN = 8;
+  osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+  osc_init.PLL.PLLR = RCC_PLLR_DIV2;
+  HAL_RCC_OscConfig(&osc_init);
+
+  RCC_ClkInitTypeDef clk_init = { 0 };
+  clk_init.ClockType =
+    RCC_CLOCKTYPE_SYSCLK |
+    RCC_CLOCKTYPE_HCLK |
+    RCC_CLOCKTYPE_PCLK1;
+  clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  clk_init.APB1CLKDivider = RCC_HCLK_DIV1;
+  HAL_RCC_ClockConfig(&clk_init, FLASH_LATENCY_2);
+
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+
+  // ======== I2C ========
+  gpio_init.Pin = GPIO_PIN_11 | GPIO_PIN_12;
+/*
+  // Test direct open-drain output
+  gpio_init.Mode = GPIO_MODE_OUTPUT_OD;
+  gpio_init.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &gpio_init);
+  while (1) {
+    static bool parity = 0;
+    HAL_GPIO_WritePin(LED_IND_ACT_PORT, LED_IND_ACT_PIN, parity);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, parity);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, parity ^= 1);
+    HAL_Delay(3000);
+  }
+*/
+  gpio_init.Mode = GPIO_MODE_AF_PP;
+  gpio_init.Alternate = GPIO_AF6_I2C2;
+  gpio_init.Pull = GPIO_NOPULL;
+  gpio_init.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &gpio_init);
+
+  __HAL_RCC_I2C2_CLK_ENABLE();
+  HAL_I2CEx_EnableFastModePlus(I2C_FASTMODEPLUS_I2C2);
+  i2c2 = (I2C_HandleTypeDef){
+    .Instance = I2C2,
+    .Init = {
+      // RM0454 Rev 5, pp. 711, 726, 738 (examples), 766
+      // APB = 64 MHz, fast mode f_SCL = 100 kHz
+      // PRESC = 15, SCLDEL = 0x4, SDADEL = 0x2,
+      // SCLH = 0x0F, SCLH = 0x0F, SCLL = 0x13
+      .Timing = 0xF0420F13,
+      .OwnAddress1 = 0x00,
+      .AddressingMode = I2C_ADDRESSINGMODE_7BIT,
+    },
+  };
+  HAL_I2C_Init(&i2c2);
+
+  // ======== Main loop ========
+  while (true) {
+    HAL_GPIO_WritePin(LED_IND_ACT_PORT, LED_IND_ACT_PIN, 1); HAL_Delay(500);
+    HAL_GPIO_WritePin(LED_IND_ACT_PORT, LED_IND_ACT_PIN, 0); HAL_Delay(500);
+  }
+}
+
+void SysTick_Handler()
+{
+  HAL_IncTick();
+  HAL_SYSTICK_IRQHandler();
+}
