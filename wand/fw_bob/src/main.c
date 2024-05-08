de2b634308e215cef59e58e5c6dbf18a09119b23
Ayu <claire@ayu.land>
2024-05-08 20:02:42 +0800

    🚧 Revise firmware

---
+++ wand/fw_bob/src/main.c
@@ -0,0 +1,128 @@
+#include <stm32l0xx_hal.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+// #define RELEASE
+#ifndef RELEASE
+#define _release_inline
+static uint8_t swv_buf[256];
+static size_t swv_buf_ptr = 0;
+__attribute__ ((noinline, used))
+void swv_trap_line()
+{
+  *(volatile char *)swv_buf;
+}
+static inline void swv_putchar(uint8_t c)
+{
+  // ITM_SendChar(c);
+  if (c == '\n') {
+    swv_buf[swv_buf_ptr >= sizeof swv_buf ?
+      (sizeof swv_buf - 1) : swv_buf_ptr] = '\0';
+    swv_trap_line();
+    swv_buf_ptr = 0;
+  } else if (++swv_buf_ptr <= sizeof swv_buf) {
+    swv_buf[swv_buf_ptr - 1] = c;
+  }
+}
+static void swv_printf(const char *restrict fmt, ...)
+{
+  char s[256];
+  va_list args;
+  va_start(args, fmt);
+  int r = vsnprintf(s, sizeof s, fmt, args);
+  for (int i = 0; i < r && i < sizeof s - 1; i++) swv_putchar(s[i]);
+  if (r >= sizeof s) {
+    for (int i = 0; i < 3; i++) swv_putchar('.');
+    swv_putchar('\n');
+  }
+}
+#else
+#define _release_inline inline
+#define swv_printf(...)
+#endif
+
+int main()
+{
+  HAL_Init();
+
+  // ======== GPIO ========
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  GPIO_InitTypeDef gpio_init;
+
+  // SWD (SWDIO PA13, SWCLK PA14)
+  gpio_init.Pin = GPIO_PIN_13 | GPIO_PIN_14;
+  gpio_init.Mode = GPIO_MODE_AF_PP; // Pass over control to AF peripheral
+  gpio_init.Alternate = GPIO_AF0_SWDIO;
+  gpio_init.Pull = GPIO_PULLUP;
+  gpio_init.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &gpio_init);
+
+  // ======== Clocks ========
+  RCC_OscInitTypeDef osc_init = { 0 };
+  osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  osc_init.HSIState = RCC_HSI_ON;
+  osc_init.PLL.PLLState = RCC_PLL_ON;
+  osc_init.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  osc_init.PLL.PLLMUL = 2;
+  HAL_RCC_OscConfig(&osc_init);
+
+  RCC_ClkInitTypeDef clk_init = { 0 };
+  clk_init.ClockType =
+    RCC_CLOCKTYPE_SYSCLK |
+    RCC_CLOCKTYPE_HCLK |
+    RCC_CLOCKTYPE_PCLK1;
+  clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  clk_init.APB1CLKDivider = RCC_HCLK_DIV1;
+  HAL_RCC_ClockConfig(&clk_init, FLASH_LATENCY_0);
+
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+
+  gpio_init.Pull = GPIO_NOPULL;
+  gpio_init.Speed = GPIO_SPEED_FREQ_HIGH;
+  gpio_init.Pin = GPIO_PIN_0;
+  gpio_init.Mode = GPIO_MODE_OUTPUT_PP;
+  HAL_GPIO_Init(GPIOA, &gpio_init);
+  while (1) {
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, 0); HAL_Delay(500);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, 1); HAL_Delay(500);
+  }
+}
+
+void SysTick_Handler()
+{
+  HAL_IncTick();
+  HAL_SYSTICK_IRQHandler();
+}
+
+void NMI_Handler() { while (1) { } }
+void HardFault_Handler() { while (1) { } }
+void SVC_Handler() { while (1) { } }
+void PendSV_Handler() { while (1) { } }
+void WWDG_IRQHandler() { while (1) { } }
+void RTC_TAMP_IRQHandler() { while (1) { } }
+void FLASH_IRQHandler() { while (1) { } }
+void RCC_IRQHandler() { while (1) { } }
+void EXTI0_1_IRQHandler() { while (1) { } }
+void EXTI2_3_IRQHandler() { while (1) { } }
+void EXTI4_15_IRQHandler() { while (1) { } }
+void DMA1_Channel1_IRQHandler() { while (1) { } }
+void DMA1_Channel2_3_IRQHandler() { while (1) { } }
+void DMA1_Ch4_5_DMAMUX1_OVR_IRQHandler() { while (1) { } }
+void ADC1_IRQHandler() { while (1) { } }
+void TIM1_BRK_UP_TRG_COM_IRQHandler() { while (1) { } }
+void TIM1_CC_IRQHandler() { while (1) { } }
+void TIM3_IRQHandler() { while (1) { } }
+void TIM14_IRQHandler() { while (1) { } }
+void TIM16_IRQHandler() { while (1) { } }
+void TIM17_IRQHandler() { while (1) { } }
+void I2C1_IRQHandler() { while (1) { } }
+void I2C2_IRQHandler() { while (1) { } }
+void SPI1_IRQHandler() { while (1) { } }
+void SPI2_IRQHandler() { while (1) { } }
+void USART1_IRQHandler() { while (1) { } }
+void USART2_IRQHandler() { while (1) { } }
